40e2492d3838c232473b47d9a23e0aa0790ebef7
Fajar Tri Cahyono <fajartc02@gmail.com>
2018-07-25 13:39:12 +0700

    promise-countdown

--- controllers.js
+++ controllers.js
@@ -1,21 +1,39 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds =>
-  new Promise((resolve, reject) => {
+function timer(seconds) {
+  return new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
-    displayCountdown(seconds - 1);
-    setInterval(() => {
+    let count = setInterval(() => {
+      displayCountdown(seconds - 1);
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
+      // console.log(secondsLeft);
       if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
+        clearInterval(count)
+        reject();
       } else {
-        displayCountdown(secondsLeft);
+        var mins;
+        var sec;
+        if (seconds >= 60) {
+          mins = Math.floor(seconds / 60);
+          sec = seconds % 60;
+          if (mins < 10) {
+            mins = '0' + mins;
+          }
+        } else {
+          sec = seconds;
+          mins = '00';
+        }
+        if (sec < 10) {
+          sec = '0' + sec;
+        }
+        let countDown = `${mins} : ${sec}`;
+        resolve(countDown)
       }
+      seconds--
     }, 1000);
   });
+}
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -2,10 +2,12 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+function displayCountdown(seconds) {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
-};
+  console.log(chalk.cyan(figlet.textSync(seconds)));
+  
+}
 
 module.exports = { displayCountdown };

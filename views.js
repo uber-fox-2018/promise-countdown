08cfe3ff7162f43656932ed8ac775e2fe1207903
joanlamrack <joanlamrack@gmail.com>
2018-07-25 16:33:54 +0700

    added

--- views.js
+++ views.js
@@ -3,9 +3,12 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
-  clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+	//clearing purposes
+	clear();
+	console.clear();
+	process.stdout.write('\033c\033[3J');
+	console.log(chalk.cyan(figlet.textSync("Countdown App")));
+	console.log(chalk.red(figlet.textSync(`${(Math.floor(seconds/60)<10?0:"")}${Math.floor(seconds/60)}:${(seconds%60<10?0:"")}${seconds%60}`)));
 };
 
 module.exports = { displayCountdown };

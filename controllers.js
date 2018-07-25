7360460b0b2d0c7e2efb7d9c450b5452c24e37ff
wisnugautama <ngurahwisnugautama@gmail.com>
2018-07-25 14:08:08 +0700

    done

--- controllers.js
+++ controllers.js
@@ -6,15 +6,33 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let countdown = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
+      let menit = Math.floor(seconds/60)
+      let detik = seconds%60
+
       if (secondsLeft < 0) {
         clearInterval();
         resolve();
       } else {
         displayCountdown(secondsLeft);
       }
-    }, 1000);
+
+      if (seconds === 0) {
+        clearInterval(countdown)
+      }
+  
+      if (detik < 10) {
+        detik = `0`+ detik
+      }
+      
+      if (menit < 10) {
+        menit = `0` + menit
+      }
+  
+      displayCountdown(`${menit}: ${detik}`)
+      seconds--
+    }, 100);
   });
 
 module.exports = {

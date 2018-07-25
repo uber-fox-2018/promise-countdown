ff1099f6ca19d5a573250d1f51049ba914bfab46
minzarddillah <minzard.dillah@gmail.com>
2018-07-25 15:07:24 +0700

    hitung mundur

--- controllers.js
+++ controllers.js
@@ -1,20 +1,52 @@
 const { displayCountdown } = require("./views");
-
+function sleep (milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 const timer = seconds =>
   new Promise((resolve, reject) => {
     const now = Date.now();
     const then = now + seconds * 1000;
-
-    displayCountdown(seconds - 1);
-    setInterval(() => {
-      const secondsLeft = Math.floor((then - Date.now()) / 1000);
-      if (secondsLeft < 0) {
-        clearInterval();
-        resolve();
-      } else {
-        displayCountdown(secondsLeft);
+    let detik = seconds
+    var menit = parseInt(seconds/60,10)
+    var waktu = parseInt(seconds%60,10)
+    // console.log(seconds);
+    setTimeout(function(){}, 1000);
+    for(let i = 0 ; i <= seconds ; i++){
+      sleep()
+      if(waktu === 0){
+        menit-=1
+        waktu = 60
+      }else if(waktu < 10 && menit < 10){
+        displayCountdown(`0${menit}:0${waktu}`);
+      }else if(waktu < 10 && menit > 10){
+        displayCountdown(`${menit}:0${waktu}`);
+      }else if(waktu > 10 && menit < 10){
+        displayCountdown(`0${menit}:${waktu}`);
+      }else{
+        displayCountdown(`${menit}:${waktu}`);
+        // console.log(menit,waktu);
       }
-    }, 1000);
+      if(i === seconds){
+        displayCountdown(`00:00`);
+      }
+      
+      waktu--
+    }
+    
+    // setInterval(() => {
+    //   const secondsLeft = Math.floor((then - Date.now()) / 1000);
+    //   if (secondsLeft < 0) {
+    //     clearInterval();
+    //     resolve();
+    //   } else {
+    //     displayCountdown(secondsLeft);
+    //   }
+    // }, 1000);
   });
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,14 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  // setTimeout(function(){}, 1000);
+  console.log(chalk.cyan(figlet.textSync(seconds)));
+  // if(seconds === 0){  
+  //   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  //   console.log(chalk.cyan(figlet.textSync(seconds)));
+  //   console.log(chalk.cyan(figlet.textSync('TIME OUT')));
+  // }
+  
 };
 
 module.exports = { displayCountdown };

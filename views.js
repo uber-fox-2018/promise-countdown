e041fe492d51aafd79208f5975fe8622ed562730
Susan Nio <susan.wen88@gmail.com>
2018-07-25 19:29:35 +0700

    Adding promise-countdown

--- views.js
+++ views.js
@@ -5,7 +5,15 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  
+  let minute = parseInt(seconds / 60, 10)
+  let second = parseInt(seconds % 60, 10);
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+  console.log(chalk.magenta(figlet.textSync(minute + ":" + second)));
+  if (seconds === 0) {
+    console.log(chalk.yellow(figlet.textSync("Happy Coding")));
+  }
 };
 
 module.exports = { displayCountdown };

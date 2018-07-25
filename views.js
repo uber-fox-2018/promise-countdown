673ba47e7a0c3856677712c29cd785925651ffca
andryjns <andry_jns@yahoo.com>
2018-07-25 12:55:33 +0700

    done..ajns

--- views.js
+++ views.js
@@ -6,6 +6,10 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  var minutes = Math.floor(seconds/60)
+  var detik = Math.round(((seconds/60) - Math.floor(seconds/60)) * 60)
+  console.log(chalk.cyan(figlet.textSync(`${minutes}:${detik}`)))
+
 };
 
 module.exports = { displayCountdown };

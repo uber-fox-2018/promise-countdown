fe8a2b4a1d52a52b7050dd208c304373451c0dfb
Andre Sudi Yudatama <andresudi@Andres-MacBook-Pro.local>
2018-07-28 16:33:03 +0700

    promise countdown

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const { timer } = require("./controllers");
 const main = () => {
   const seconds = parseInt(process.argv[2]) || 10;
   // Your code here...
+  timer(seconds)
+  .then(data => {
+    displayCountdown(data)
+  })
+  .catch(err => {
+    displayCountdown(err)
+  })
 };
 
 main();
--- controllers.js
+++ controllers.js
@@ -6,15 +6,34 @@ const timer = seconds =>
     const then = now + seconds * 1000;
 
     displayCountdown(seconds - 1);
-    setInterval(() => {
+    let countdown = setInterval(() => {
       const secondsLeft = Math.floor((then - Date.now()) / 1000);
+     
       if (secondsLeft < 0) {
         clearInterval();
         resolve();
       } else {
         displayCountdown(secondsLeft);
       }
-    }, 1000);
+
+      let minute = Math.trunc(seconds/60)
+      let sec = seconds%60
+      
+      if (seconds === 0) {
+        clearInterval(countdown)
+      }
+
+      if (sec < 10) {
+        sec = '0' + sec 
+      } 
+
+      if (minute < 10) {
+        minute = '0' + minute
+      }
+      
+      displayCountdown(`${minute} : ${sec}`)
+      seconds--
+    }, 100);
   });
 
 module.exports = {
